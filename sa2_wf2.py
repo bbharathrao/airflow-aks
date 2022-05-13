006d47ea4379c7eb37aadcd2c1d5b5aec9aeed13
Bharath Rao <30913392+bbharathrao@users.noreply.github.com>
2022-05-13 00:04:47 -0500

    Update sa2_wf2.py

--- sa2_wf2.py
+++ sa2_wf2.py
@@ -35,7 +35,7 @@ with DAG(
     t3 = BashOperator(
         task_id='get note book details',
         depends_on_past=False,
-        bash_command=sleep 5,
+        bash_command='sleep 5',
     )
 
     t4 = BashOperator(
@@ -53,7 +53,7 @@ with DAG(
     t6 = BashOperator(
         task_id='Email Status Notification',
         depends_on_past=False,
-        bash_command=sleep 5,
+        bash_command='sleep 5',
     )
 
 
