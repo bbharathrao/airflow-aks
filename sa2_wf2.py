b8c0a7d179e6c9bae486e1a14671cbd8a3e33ad2
Bharath Rao <30913392+bbharathrao@users.noreply.github.com>
2022-05-25 08:14:22 -0500

    Update sa2_wf2.py

--- sa2_wf2.py
+++ sa2_wf2.py
@@ -14,7 +14,7 @@ with DAG(
         'retry_delay': timedelta(minutes=5)
     },
     description='LCB Tivoli Schedule 2',
-    schedule_interval=timedelta(days=1),
+    schedule_interval='0 16 * * *',
     start_date=datetime(2021, 1, 1),
     catchup=False,
     tags=['example'],
