1a6b4f2b5a72771921e9f3b43986fc2e3194e302
Bharath Rao <30913392+bbharathrao@users.noreply.github.com>
2022-05-25 01:31:59 -0500

    Update sa1_wf1.py

--- sa1_wf1.py
+++ sa1_wf1.py
@@ -2,9 +2,13 @@ from datetime import datetime, timedelta
 from textwrap import dedent
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.contrib.sensors.file_sensor import FileSensor
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 with DAG(
-    'subject-area-1-work-flow-1',
+    'ODY-TS1-DEV',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
@@ -13,55 +17,48 @@ with DAG(
         'retries': 1,
         'retry_delay': timedelta(minutes=5)
     },
-    description='Subject Area 1 DAG Work Flow 1',
-    schedule_interval=timedelta(days=1),
+    description='ODY Tivoli SChedule 1',
+    schedule_interval='0 16 * * *',
     start_date=datetime(2021, 1, 1),
     catchup=False,
     tags=['example'],
 ) as dag:
 
-    t1 = BashOperator(
-        task_id='check_for_source_files',
-        bash_command='date',
+    t1 = FileSensor(
+        task_id='FileWatcher',
+        poke_interval=30
     )
 
-    t2 = BashOperator(
-        task_id='source_schema_check',
+    t2 = PythonOperator(
+        task_id='DataStage1',
         depends_on_past=False,
         bash_command='sleep 5',
         retries=3,
     )
 
-    t3 = BashOperator(
-        task_id='get_note_book_details',
-        depends_on_past=False,
-        bash_command='sleep 5',
-    )
-
-    t4 = BashOperator(
-        task_id='trigger_note_book_Transform_1',
-        bash_command='date',
-    )
-
-    t5 = BashOperator(
-        task_id='trigger_note_book_Transform_2',
+    t3 = PythonOperator(
+        task_id='DataStage2',
         depends_on_past=False,
         bash_command='sleep 5',
         retries=3,
+    )
+
+    t4 = DummyOperator(
+        task_id='OtherTask',
+        retries=3
+    )
+    
+    t5 = trigger_dependent_dag = TriggerDagRunOperator(
+        task_id="trigger_dependent_dag",
+        trigger_dag_id="dependent-dag",
+        wait_for_completion=True
     )
 
     t6 = BashOperator(
-        task_id='check_target_schema',
-        depends_on_past=False,
-        bash_command='sleep 5',
-        retries=3,
-    )
-
-    t7 = BashOperator(
-        task_id='Email_Status_Notification',
+        task_id='EmailNotification',
         depends_on_past=False,
         bash_command='sleep 5',
     )
 
 
-    t1 >> t2 >> t3 >> t4 >> t5 >> t6 >> t7
+    t1 >> t2 >> t3 >> t4 >> t5 >> t6
