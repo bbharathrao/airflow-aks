07be53c861ba3049916c287ea4028789833567e2
bharathrao-aa <105375357+bharathrao-aa@users.noreply.github.com>
2022-06-16 12:16:56 -0500

    Create submit_airflow_databricks.py

---
+++ submit_airflow_databricks.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
+from datetime import datetime, timedelta 
+
+
+notebook_task = {
+    'notebook_path': '/Users/211531@corpaa.aa.com/test_sunny',
+}
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2)
+}
+
+with DAG('databricks_dag',
+    start_date=datetime(2022, 6, 15),
+    schedule_interval='@daily',
+    catchup=False,
+    default_args=default_args
+    ) as dag:
+
+    opr_submit_run = DatabricksSubmitRunOperator(
+        task_id='submit_run',
+        databricks_conn_id='aa-databricks-test',
+        existing_cluster_id: 'n-tst09-adbde-cl-001',
+        notebook_task=notebook_task
+    )
+
+    opr_submit_run
